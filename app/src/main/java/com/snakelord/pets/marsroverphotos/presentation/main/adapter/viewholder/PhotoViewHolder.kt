146923e32ea59b38587e5062ce91c9372922d044
snakelord757 <blackkiller1527@gmail.com>
2021-05-14 21:53:18 +0300

    Оптимизировал импорт в PhotoViewHolder

--- app/src/main/java/com/snakelord/pets/marsroverphotos/presentation/main/adapter/viewholder/PhotoViewHolder.kt
+++ app/src/main/java/com/snakelord/pets/marsroverphotos/presentation/main/adapter/viewholder/PhotoViewHolder.kt
@@ -3,12 +3,12 @@ package com.snakelord.pets.marsroverphotos.presentation.main.adapter.viewholder
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.snakelord.pets.marsroverphotos.R
 import com.snakelord.pets.marsroverphotos.data.model.Photo
 import com.squareup.picasso.Picasso
 
-class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class PhotoViewHolder(itemView: View) : ViewHolder(itemView) {
 
     private val photoImageView = itemView.findViewById<ImageView>(R.id.photo)
     private val roverNameTextView = itemView.findViewById<TextView>(R.id.roverName)
