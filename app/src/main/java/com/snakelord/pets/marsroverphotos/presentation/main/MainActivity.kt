52b3c3624e17b26e43640adef3081a81b1f63fc0
snakelord757 <blackkiller1527@gmail.com>
2021-06-02 13:41:37 +0300

    Пакет main: -Удалил MainViewModel -Вынес инициализацию navController'а в отдельную функцию

--- app/src/main/java/com/snakelord/pets/marsroverphotos/presentation/main/MainActivity.kt
+++ app/src/main/java/com/snakelord/pets/marsroverphotos/presentation/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.snakelord.pets.marsroverphotos.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.snakelord.pets.marsroverphotos.R
@@ -12,17 +13,19 @@ class MainActivity : AppCompatActivity() {
     private var mainActivityBinding: ActivityMainBinding? = null
     private val binding
         get() = mainActivityBinding!!
-    private val navController by lazy {
-        (supportFragmentManager
-            .findFragmentById(R.id.fragmentContainer) as NavHostFragment)
-            .navController
-    }
+    private val navController by lazy { initNavController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+    }
+
+    private fun initNavController(): NavController {
+        return (supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer) as NavHostFragment)
+            .navController
     }
 
 }
